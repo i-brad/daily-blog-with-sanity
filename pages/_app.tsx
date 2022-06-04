706a2c09d22071bdf901d375d348a1bf8316d1d3
MickelsonKlaus <klausmickelson52@email.com>
2022-06-04 15:13:54 +0100

    adding google adsense

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -15,6 +15,8 @@ function MyApp({ Component, pageProps }: AppProps) {
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
         <meta name="msapplication-TileColor" content="#da532c" />
         <meta name="theme-color" content="#ffffff" />
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9998565672672812"
+          crossOrigin="anonymous"></script>
       </Head>
       <DefaultSeo title='Daily - News, tips, updates and more' description='Get news ranging from different topics like, technology, design, hacks, business and more' canonical='https://dailyposts.vercel.app/' openGraph={{
         type: 'website',
